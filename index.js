f227a1cb3cd52e60b4b0a7fda2a5579b838a93a7
noor <noorridha028@gmail.com>
2022-10-23 20:31:28 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2
+console.log(multiply)
+let random = Math.round(Math.random());
+console.log(random);
+let num3 = 16
+let num4 = 6
+let mod = num3 % num4;
+console.log(mod);
+let max = Math.max(5, 17, 20, 14);
+console.log(max);
